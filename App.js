dff52b939d84a21d937192730a1ee5010a471951
Roberto <roberto.quadraccia90@gmail.com>
2024-08-27 11:10:45 +0200

    Demo app complete, push notifications implemented

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Alert, View, Platform } from "react-native";
 import * as Notifications from "expo-notifications";
-// import * as Permissions from "expo-permissions";
+import Constants from "expo-constants";
+
+// IMPORTANT: If building the app without Expo Go, additional configuration is necessary
 
 // 1 - We import * as Notifications from "expo-notifications"
 // 2 - We then call Notifications.scheduleNotificationAsync() inside scheduleNotificationHandler to schedule a local notification.
@@ -22,7 +24,28 @@ import * as Notifications from "expo-notifications";
 // is removed from the screen, when it's not active anymore. We do that by return a function and passing it inside.
 
 // 8 - To listen to user interaction with our notifications, we use Notifications.addNotificationResponseReceivedListener inside the same useEffect.
-//
+
+// 9 - Now we will implement push notifications (local until now.). We again go through useEffect at the beginning of the App.
+// We use Notifications.getExpoPushTokenAsync() to get the push token for this device.
+
+// NOTE: You will need the projectId to be passed inside getExpoPushTokenAsync(). 
+// I had to use:
+// npm install -g eas-cli
+// eas login
+
+// IMPORTANT: Push token will be the address, which we will need later when we send push notifications to other devices
+// because, when it comes to delivering push notifications to other devices, you need some way of identifying devices.
+// The address of a device is simply such a push token, which will be unique for every device.
+
+// 10 - We create an helper function configurePushNotification where we use Notifications.getPermissionsAsync() to get permission for iOS (Android does this automatically)
+// and Notifications.requestPermissionsAsync(); to request permission if we don't have it yet.
+// 11 - We then call configurePushNotification() at the end of the useEffect.
+// 12 - We check if Platform.OS === 'android', in which case we need some further configuration.
+// Notifications.setNotificationChannelAsync("default", {name: "default", importance: Notifications.AndroidImportance.DEFAULT});
+
+// IMPORTANT: https://expo.dev/notifications allows you to test a notification
+// 14 - We create sendPushNotificationHandler and attach it to the button.
+// Inside we do a POST request to the endpoint and configure the fetch as shown below.
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
@@ -36,20 +59,44 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   useEffect(() => {
-    async function requestPermissions() {
-      const { status } = await Notifications.requestPermissionsAsync();
-      if (status !== 'granted') {
-        alert('Permission to send notifications was not granted');
+    const configurePushNotification = async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+
+      if (finalStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus;
       }
-    }
-    requestPermissions();
+
+      if (finalStatus !== "granted") {
+        Alert.alert(
+          "Permission required",
+          "Push notifications need the appropriate permissions."
+        );
+        return;
+      }
+      const pushTokenData = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig.extra.eas.projectId,
+      });
+
+      console.log(pushTokenData);
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.DEFAULT,
+        });
+      }
+    };
+
+    configurePushNotification();
   }, []);
 
   useEffect(() => {
     const subscription1 = Notifications.addNotificationReceivedListener(
       (notification) => {
         console.log("NOTIFICATION RECEIVED");
-        console.log(notification.request.content.data);
+        console.log(notification.request.content);
       }
     );
 
@@ -79,11 +126,29 @@ export default function App() {
     });
   };
 
+  const sendPushNotificationHandler = () => {
+    fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: "ExponentPushToken[qZXPOgH05nUi8e_FKSST9h]",
+        title: "Test - sent from a device!",
+        body: "This is a test!",
+      }),
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Button
         title="Schedule Notification"
         onPress={scheduleNotificationHandler}
+      />
+      <Button
+        title="Send Push Notification"
+        onPress={sendPushNotificationHandler}
       />
       <StatusBar style="auto" />
     </View>
